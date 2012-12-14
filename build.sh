27f8171290cfc6767b338165335212cf7e2094a6
Thomas Kapoulas <thomas.kapoulas@gmail.com>
2012-12-14 15:27:20 +0200

    build script changed

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-a2x -f xhtml --icons -r images --xsl-file=docbook-xsl/xhtml.xsl openbook.asciidoc 
+a2x -f xhtml --icons -r images --xsl-file=docbook-xsl/xhtml.xsl openbook.asciidoc;
+mv openbook.html index.html;
